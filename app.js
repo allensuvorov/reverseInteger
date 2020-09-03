07434b11eb1625949af8538a36e7a033f6430ee7
Allen <allenvs@mail.ru>
2020-09-03 22:32:06 +0300

    js solution ready

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-console.log (reverse(123));
-// console.log (reverse(-12345));
-// console.log ((-234)%10);
-console.log (10);
+
 /*
- @param {number} x
- @return {number}
- */
+@param {number} x
+@return {number}
+*/
+
+console.log (reverse(123));
+console.log (reverse(-12345));
 
 function reverse(x) {
     
     let rx = 0; //reversed x
-    
-    for (let int = x; Math.abs(int) > 0; int = Math.floor(int/10)) {
+    for (let int = x; Math.abs(int) > 0; int = Math.trunc(int/10)) {
         rx *= 10;
-        rx += int % 10;
+        rx += int % 10; // take the end digit of int and add to the reversed x 
+        // console.log("stack: ",rx, int)
     };
-    
     return rx;
 };
