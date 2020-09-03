36d4935c5fc06a0685045bc72758cd2e1b2e6ae7
Allen <allenvs@mail.ru>
2020-09-03 21:57:22 +0300

    need to reload

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 console.log (reverse(123));
-console.log (reverse(-12345));
-
+// console.log (reverse(-12345));
+// console.log ((-234)%10);
+console.log (10);
 /*
  @param {number} x
  @return {number}
@@ -10,7 +11,7 @@ function reverse(x) {
     
     let rx = 0; //reversed x
     
-    for (let int = x; int > 0; int = Math.floor(int/10)) {
+    for (let int = x; Math.abs(int) > 0; int = Math.floor(int/10)) {
         rx *= 10;
         rx += int % 10;
     };
